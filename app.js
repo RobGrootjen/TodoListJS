2427a7a650b26735ace63fbc3703462e2b831e78
Rob Grootjen <robgrootjen1@gmail.com>
2021-01-18 16:58:38 -0600

    new push

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
-function getInput(){
-    var x = document.getElementById("inputs").value;
-    console.log(x);
+var entry = document.getElementById("entry");
+entry.addEventListener("click",displayDetails);
+
+var row = 1;
+
+function displayDetails(){
+    var taskNow = document.getElementById("task").value;
+    var timeNow = document.getElementById("time").value;
+    var dateNow = document.getElementById("date").value;
+
+    if (!taskNow || !timeNow || !dateNow) {
+        alert("Please fill all the boxes!")
+        return;
+    }
+
+    var display = document.getElementById("display");
+
+    var newRow = display.insertRow(row);
+
+    var cell1 = newRow.insertCell(0);
+    var cell2 = newRow.insertCell(1);
+    var cell3 = newRow.insertCell(2);
+
+    cell1.innerHTML = taskNow;
+    cell2.innerHTML = timeNow;
+    cell3.innerHTML = dateNow;
+
+    row++;
 }
